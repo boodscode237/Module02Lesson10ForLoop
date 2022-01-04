66ff776856026aca168de97badb5e84171754c65
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-04 16:25:41 +0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] firstNames = new string[] { "Joe", "John", "Doe" };
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                Console.WriteLine(firstNames[i]);
+            }
+
+            List<string> lastnames = new List<string>();
+            lastnames.Add("Johanne");
+            lastnames.Add("Brido");
+            lastnames.Add("Donald");
+
+            for (int i = 0; i < lastnames.Count; i++)
+            {
+                Console.WriteLine(lastnames[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
